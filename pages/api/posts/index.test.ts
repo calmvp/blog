92f52f9476c9ca9e2ce93530193c058f6305a698
Ned Kaida-Yip <ned.kaida-yip@spr.com>
2023-04-28 15:16:12 -0500

    get request tests in progress

--- pages/api/posts/index.test.ts
+++ pages/api/posts/index.test.ts
@@ -1,11 +1,12 @@
 import { createMocks, createRequest, createResponse, RequestMethod } from 'node-mocks-http';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import handler from './index';
-import { MongoClient } from 'mongodb';
+import mongodb, { MongoClient } from 'mongodb';
 
 type ApiRequest = NextApiRequest & ReturnType<typeof createRequest>;
 type ApiResponse = NextApiResponse & ReturnType<typeof createResponse>;
 
+jest.mock('mongodb');
 describe('/api/posts API Endpoint', () => {
   afterEach(() => {
     jest.resetAllMocks();
@@ -20,8 +21,7 @@ describe('/api/posts API Endpoint', () => {
     return { req, res};
   }
 
-  it('should return a 405 is HTTP method is not GET or POST', async () => {
-    const connectSpy = jest.spyOn(MongoClient, 'connect');
+  it('should return a 405 if HTTP method is not GET or POST', async () => {
     const { req, res} = mockRequestResponse('PUT');
 
     await handler(req, res);
@@ -30,5 +30,45 @@ describe('/api/posts API Endpoint', () => {
     expect(res._getJSONData()).toEqual({
       err: 'Method not allowed'
     });
-  })
+  });
+
+  it('should return a 500 on failure to connect to database', async () => {
+    // arrange
+    const { req, res }  = mockRequestResponse('GET');
+    const connectSpy = jest.spyOn(MongoClient, 'connect').mockRejectedValueOnce(new Error('failed'));
+
+    // act
+    await handler(req, res);
+
+    // assert
+    expect(connectSpy).toHaveBeenCalled();
+    expect(res.statusCode).toBe(500);
+    expect(res._getJSONData()).toEqual({
+      err: 'Failed to connect to database'
+    });
+  });
+  describe('GET request method', () => {
+    it('should call the .find on the database with no args if featured query param is falsey', async () => {
+      // arrange
+      const { req, res } = mockRequestResponse('GET');
+      const client = { db: jest.fn().mockReturnThis() }
+      const connectSpy = jest.spyOn(MongoClient, 'connect')
+        .mockResolvedValueOnce(({ 
+          db: jest.fn().mockReturnValue(
+            {
+              find: jest.fn().mockResolvedValueOnce([])
+            }
+          ),
+          close: jest.fn()
+        } as unknown) as MongoClient);
+      
+      // act
+      await handler(req, res);
+
+      // assert
+      expect(connectSpy).toHaveBeenCalled();
+      expect(res.statusCode).toBe(200);
+      expect(res._getJSONData).toEqual([]);
+    });
+  });
 });
--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,5 +1,3 @@
-global.TextEncoder = require('util').TextEncoder;
-global.TextDecoder = require('util').TextDecoder;
 import type { NextApiRequest, NextApiResponse } from "next";
 import { MongoClient } from "mongodb";
 
@@ -10,67 +8,70 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  // const client = await getClient(res);
+  const client = await getClient(res);
 
-  // if (!client) {
-  //   res.status(404).json({ message: 'Could not connect to db' });
-  //   return;
-  // }
+  if (!client) {
+    return;
+  }
 
-  // const db = client.db();
+  const db = client.db();
 
-  // if (req.method === 'GET') {
-  //   try {
-  //     const { featured } = req.query;
-  //     let posts;
+  if (req.method === 'GET') {
+    try {
+      const { featured } = req.query;
+      let posts;
       
-  //     if (featured) {
-  //       posts = await db.collection('posts')
-  //         .find( { isFeatured: true })
-  //         .sort( { _id: -1})
-  //         .toArray();
-  //     } else {
-  //       posts = await db.collection('posts')
-  //       .find()
-  //       .sort({ _id: -1 })
-  //       .toArray();
-  //     }
+      if (featured) {
+        posts = await db.collection('posts')
+          .find( { isFeatured: true })
+          .sort( { _id: -1})
+          .toArray();
+      } else {
+        posts = await db.collection('posts')
+        .find()
+        .sort({ _id: -1 })
+        .toArray();
+      }
       
-  //     client.close();
-  //     res.status(200).json(posts)
-  //   } catch (err) {
-  //     res.status(400).json({err: JSON.stringify(err)});
-  //     return;
-  //   }
-  // }
-  // if (req.method === 'POST') {
-  //   try {
-  //     const { posts } = req.body;
+      client.close();
+      res.status(200).json(posts)
+    } catch (err) {
+      res.status(400).json({err: JSON.stringify(err)});
+      return;
+    }
+  }
+  if (req.method === 'POST') {
+    try {
+      const { posts } = req.body;
 
-  //     const db = client.db();
-  //     await db.collection('posts').insertMany(posts);
-  //     client.close();
+      const db = client.db();
+      await db.collection('posts').insertMany(posts);
+      client.close();
 
-  //     res.status(201).json({ message: 'Success!' });
-  //     return;
-  //   } catch (err) {
-  //     res.status(400).json({err: JSON.stringify(err)});
-  //     return;
-  //   }
-  // };
+      res.status(201).json({ message: 'Success!' });
+      return;
+    } catch (err) {
+      res.status(400).json({err: JSON.stringify(err)});
+      return;
+    }
+  };
 }
 
 const getClient = async (res: NextApiResponse) => {
   const { MONGO_BASE_URL, MONGOUSER, PASSWORD } = process.env;
   if (MONGO_BASE_URL && MONGOUSER && PASSWORD) {
-    const endpoint = MONGO_BASE_URL.replace('<username>', MONGOUSER).replace('<password>', PASSWORD);
-    const client = await MongoClient.connect(endpoint);
-    // return client;
-  } 
-  // else {
-  //   res.status(500).json({ err: 'Missing db configuration' });
-  //   return;
-  // }
+    try {
+      const endpoint = MONGO_BASE_URL.replace('<username>', MONGOUSER).replace('<password>', PASSWORD);
+      const client = await MongoClient.connect(endpoint);
+      return client;
+    } catch(err) {
+      res.status(500).json({ err: 'Failed to connect to database'});
+      return;
+    }
+  } else {
+    res.status(500).json({ err: 'Missing db configuration' });
+    return;
+  }
 }
 
 export default handler;
