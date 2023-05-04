bb1269a6e98ac473532509b7800348e9e13dfe0c
Ned Kaida-Yip <ned.kaida-yip@spr.com>
2023-05-04 13:45:47 -0500

    adds state for form with tests

--- components/contact/contact-form.test.tsx
+++ components/contact/contact-form.test.tsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ContactForm from "./contact-form";
 
 describe('ContactForm', () => {
@@ -14,30 +15,66 @@ describe('ContactForm', () => {
     expect(actual).toBeInTheDocument();
   });
 
-  test('it should render a labelled email input', () => {
-    render(<ContactForm />);
-    const actual = screen.getByLabelText(/email/i);
-    expect(actual).toBeInTheDocument();
-    expect(actual).toHaveAttribute('type', 'email');
-  });
-
-  test('it should render a labelled name input', () => {
-    render(<ContactForm/>);
-    const actual = screen.getByLabelText(/name/i);
-    expect(actual).toBeInTheDocument();
-    expect(actual).toHaveAttribute('type', 'text');
-  });
-
-  test('it should render a labelled message textarea', () => {
-    render(<ContactForm />);
-    const actual = screen.getByLabelText(/message/i);
-    expect(actual).toBeInTheDocument();
-    expect(actual).toHaveAttribute('rows');
-  });
-
   test('it should render a "Send Message" button', () => {
     render(<ContactForm />);
     const actual = screen.getByRole('button', { name: /send message/i});
     expect(actual).toBeInTheDocument();
+  });
+
+  describe('email input', () => {
+    test('it should render a labelled email input', () => {
+      render(<ContactForm />);
+      const actual = screen.getByLabelText(/email/i);
+      expect(actual).toBeInTheDocument();
+      expect(actual).toHaveAttribute('type', 'email');
+      expect(actual).toHaveAttribute('required');
+    });
+
+    test('it should update when user enters text', async () => {
+      const expected = 'c.bellagio@gmail.com';
+      const user = userEvent.setup();
+      render(<ContactForm />);
+      const emailInput = screen.getByLabelText(/email/i);
+      await user.type(emailInput, expected);
+      expect(emailInput).toHaveValue(expected);
+    });
+  });
+
+  describe('name input', () => {
+    test('it should render a labelled name input', () => {
+      render(<ContactForm/>);
+      const actual = screen.getByLabelText(/name/i);
+      expect(actual).toBeInTheDocument();
+      expect(actual).toHaveAttribute('type', 'text');
+      expect(actual).toHaveAttribute('required');
+    });
+
+    test('it should update when user enters text', async () => {
+      const expected = 'Clint Bellagio';
+      const user = userEvent.setup();
+      render(<ContactForm />);
+      const nameInput = screen.getByLabelText(/name/i);
+      await user.type(nameInput, expected);
+      expect(nameInput).toHaveValue(expected);
+    });
+  });
+
+  describe('message text area', () => {
+    test('it should render a labelled message textarea', () => {
+      render(<ContactForm />);
+      const actual = screen.getByLabelText(/message/i);
+      expect(actual).toBeInTheDocument();
+      expect(actual).toHaveAttribute('rows');
+      expect(actual).toHaveAttribute('required');
+    });
+
+    test('it should update when user enters text', async () => {
+      const expected = 'The message I am going to send';
+      const user = userEvent.setup();
+      render(<ContactForm />);
+      const messageTextArea = screen.getByLabelText(/message/i);
+      await user.type(messageTextArea, expected);
+      expect(messageTextArea).toHaveValue(expected);
+    });
   });
 });
--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+import { ChangeEvent } from 'react';
+import axios from 'axios';
 import classes from './contact-form.module.css';
 
 const ContactForm = () => {
+  const [contactInfo, setContactInfo] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+    const newObje = {...contactInfo, [event.target.id]: event.target.value }
+    setContactInfo(newObje);
+  };
+
   return (
     <section className={classes.contact}>
       <h1>What Ails You, My Friend?</h1>
@@ -8,15 +22,33 @@ const ContactForm = () => {
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required />
+            <input 
+              type='email' 
+              id='email' 
+              value={contactInfo.email}
+              onChange={handleChange}
+              required 
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' required />
+            <input 
+              type='text' 
+              id='name' 
+              value={contactInfo.name}
+              onChange={handleChange}
+              required 
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor='message'>Your Message</label>
-            <textarea id='message' rows={5} ></textarea>
+            <textarea 
+              id='message' 
+              rows={5}
+              value={contactInfo.message}
+              onChange={handleChange}
+              required
+            ></textarea>
           </div>
         </div>
         <div className={classes.actions}>
